3f27716db5067ed49da06777267bbb3b30eeea9e
ar-rony <ar.rony87.official@gmail.com>
2021-04-29 23:54:35 +0600

    Sass enabled to the theme

--- functions.php
+++ functions.php
@@ -5,8 +5,6 @@
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
  * @package Aurno
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
  */
 
  final class Aurno{
