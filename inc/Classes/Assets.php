1fc1c08a82c8fc31b2733f43edf6fb7ae2764075
ar-rony <ar.rony87.official@gmail.com>
2021-05-11 17:24:27 +0600

    scalaton for classes

--- inc/Classes/Assets.php
+++ inc/Classes/Assets.php
@@ -8,5 +8,9 @@ class Assets {
     }
     public function blognow_scripts(){
         wp_enqueue_style('blognow-stylesheet', get_stylesheet_uri(), '', '1.0.0', 'all');
+        
+        if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+            wp_enqueue_script( 'comment-reply' );
+        }
     }
 }
